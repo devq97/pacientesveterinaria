ff714478be956f4bb9e77693b18a401497843c96
David Esteban Vargas Quevedo <david.vargas@decameron.com>
2020-01-14 13:05:53 -0500

    Se crea el componente de Lista Citas y se muestran las Citas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './bootstrap.min.css';
 import Header from "./components/Header";
 import NuevaCita from "./components/NuevaCita";
+import ListaCitas from "./components/ListaCitas";
 
 class App extends Component {
   state = {
@@ -28,6 +29,12 @@ class App extends Component {
             <div className="col-md-10 mx-auto">
               <NuevaCita
                 crearNuevaCita={this.crearNuevaCita}
+              />
+            </div>
+
+            <div className="mt-5 col-md-10 mx-auto">
+              <ListaCitas
+                citas={this.state.citas}
               />
             </div>
           </div>
